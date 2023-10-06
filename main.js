04cb3e43135f5e32f3d1f26b150f004bfe99c0c0
RobertoCassio <rcslino@gmail.com>
2023-10-05 22:15:37 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,55 @@
+$(document).ready(function(){
+    var inFeed = true;
+    var inProjects = false;
+    var inAbout = false;
+    var inContact = false;
+
+    //Load Feed When Starts
+    $("#mainbody").load("feed.html")
+    //To Feed 
+    $("#feed").click(function(){
+        $("#mainbody").load("feed.html")
+        inFeed = true;
+        inProjects = false;
+        inAbout = false;
+        inContact = false;
+        updateUnderline();
+    });
+
+    //To Projects
+    $("#projects").click(function(){
+        $("#mainbody").load("projects.html")
+        inProjects = true;
+        inFeed = false;
+        inAbout = false;
+        inContact = false;
+        updateUnderline();   
+    });
+
+    //To AboutMe
+    $("#aboutme").click(function(){
+        $("#mainbody").load("aboutme.html")
+        inAbout = true;
+        inProjects = false;
+        inFeed = false;
+        inContact = false;
+        updateUnderline();
+    });
+  
+updateUnderline();
+
+function updateUnderline(){
+    $(".nav-item").removeClass("underline-item");
+    if(inFeed === true){
+        $('#feed').addClass("underline-item");
+    }else if (inProjects === true){
+        $('#projects').addClass("underline-item");
+    }else if (inAbout === true){
+        $('#aboutme').addClass("underline-item");
+    }else if(inContact === true){
+        $('#contacts').addClass("underline-item");
+    }
+}
+
+});
+
